bc880f9b3d9b8abe312d363238521214364f1140
Jnani <jnaneshwar1234@gmail.com>
2012-10-28 20:55:26 -0700

    Added race interface

--- src/com/TeamNovus/SupernaturalRaces/Models/Race.java
+++ src/com/TeamNovus/SupernaturalRaces/Models/Race.java
@@ -1,6 +1,14 @@
 package com.TeamNovus.SupernaturalRaces.Models;
 
 import org.bukkit.event.Event;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.PlayerVelocityEvent;
 
 public interface Race {
 	String name();
@@ -8,4 +16,17 @@ public interface Race {
 	
 	void onPlayerJoinRace(Event event);
 	void onPlayerLeaveRace(Event event);
+	
+	void onPlayerMove(Event event);
+	void onPlayerDeath(PlayerDeathEvent event);
+	void onPlayerRespawn(PlayerRespawnEvent event);
+	void onPlayerVelocity(PlayerVelocityEvent event);
+	void onPlayerTeleport(PlayerTeleportEvent event);
+	void onPlayerInteract(PlayerInteractEvent event);
+	
+	void onPlayerDamage(EntityDamageEvent event);
+	
+	void onProjectileHitEvent(ProjectileHitEvent event);
+	
+	void onInventoryClick(InventoryClickEvent event);
 }
