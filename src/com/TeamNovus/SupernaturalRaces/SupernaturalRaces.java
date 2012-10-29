b8d6271203f64224004b7b58eb9ed08d9913a2ff
Jnani <jnaneshwar1234@gmail.com>
2012-10-28 20:31:23 -0700

    Added main class

---
+++ src/com/TeamNovus/SupernaturalRaces/SupernaturalRaces.java
@@ -0,0 +1,16 @@
+package com.TeamNovus.SupernaturalRaces;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SupernaturalRaces extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO: Code to execute onEnable
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO: Code to execute onDisable
+	}
+}
