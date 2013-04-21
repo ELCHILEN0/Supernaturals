a45f9ed8c7149139e4272e6dc643a8ba40c3c087
Jnani Weibel <jnaneshwar1234@gmail.com>
2013-04-20 19:32:49 -0700

    Changed KillDeathListener to only listen for player kills and deaths

--- src/main/java/com/TeamNovus/Supernaturals/Listeners/Custom/KillDeathListener.java
+++ src/main/java/com/TeamNovus/Supernaturals/Listeners/Custom/KillDeathListener.java
@@ -1,7 +1,6 @@
 package com.TeamNovus.Supernaturals.Listeners.Custom;
 
 import org.bukkit.GameMode;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,20 +14,15 @@ public class KillDeathListener implements Listener {
 	
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onEntityDeathEvent(EntityDeathEvent event) {
-		LivingEntity killed = event.getEntity();
-		Player killer = event.getEntity().getKiller();
 		
-		if(killed instanceof Player) {
-			SNPlayer player = SNPlayers.i.get((Player) killed);
+		if(event.getEntity() instanceof Player) {
+			SNPlayer killed = SNPlayers.i.get((Player) event.getEntity());
+			SNPlayer killer = SNPlayers.i.get((Player) event.getEntity().getKiller());
 
-			player.setDeaths(player.getDeaths() + 1);
-		}
-		
-		if(killer != null) {
-			SNPlayer player = SNPlayers.i.get(killer);
+			killed.setDeaths(killed.getDeaths() + 1);
 			
-			if(!(player.getPlayer().getGameMode().equals(GameMode.CREATIVE))) {
-				player.setKills(player.getKills() + 1);
+			if(!(killer.getPlayer().getGameMode().equals(GameMode.CREATIVE))) {
+				killer.setKills(killer.getKills() + 1);
 			}
 		}
 	}
