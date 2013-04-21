7cca1a4a010bc6a6afd10a87c1217ac1112ad6f8
Jnani Weibel <jnaneshwar1234@gmail.com>
2013-04-20 21:01:00 -0700

    Various tests on alternating hunger bug

--- src/main/java/com/TeamNovus/Supernaturals/Listeners/Custom/HungerListener.java
+++ src/main/java/com/TeamNovus/Supernaturals/Listeners/Custom/HungerListener.java
@@ -1,5 +1,6 @@
 package com.TeamNovus.Supernaturals.Listeners.Custom;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,21 +9,43 @@ import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
 import com.TeamNovus.Supernaturals.SNPlayers;
+import com.TeamNovus.Supernaturals.Supernaturals;
 import com.TeamNovus.Supernaturals.Player.SNPlayer;
 
 public class HungerListener implements Listener {
 
 	@EventHandler(priority=EventPriority.HIGHEST)
-	public void onFoodLevelChange(FoodLevelChangeEvent event) {
+	public void onFoodLevelChange(final FoodLevelChangeEvent event) {
 		if(event.isCancelled())
 			return;
 		
 		if(event.getEntity() instanceof Player) {
-			SNPlayer player = SNPlayers.i.get((Player) event.getEntity());
-																				
-			player.setFoodLevel(player.getFoodLevel() + (event.getFoodLevel() - ((Player) event.getEntity()).getFoodLevel()));	
+			final SNPlayer player = SNPlayers.i.get((Player) event.getEntity());
 			
-			event.setFoodLevel((int) Math.ceil(player.getFoodLevel() * 20 / player.getMaxFoodLevel()));
+			System.out.println("Before: " + player.getFoodLevel() * 20 / player.getMaxFoodLevel());
+
+			player.setFoodLevel(player.getFoodLevel() + (event.getFoodLevel() - ((Player) event.getEntity()).getFoodLevel()));	
+
+			System.out.println("After: " + player.getFoodLevel() * 20 / player.getMaxFoodLevel());
+
+			// Rescales the client food level bar.
+//			Bukkit.getScheduler().runTaskAsynchronously(Supernaturals.getPlugin(), new Runnable() {
+//				
+//				@Override
+//				public void run() {
+//					player.updateFoodLevel();
+//					
+//				}
+//			});
+			
+			int foodLevel = player.getFoodLevel() * 20 / player.getMaxFoodLevel();
+			System.out.println("food level" + foodLevel);
+			event.setFoodLevel(foodLevel);
+			
+//			event.setFoodLevel((int) Math.ceil(player.getFoodLevel() * 20.0 / player.getMaxFoodLevel()));
+
+
+			
 		}
 	}
 	
