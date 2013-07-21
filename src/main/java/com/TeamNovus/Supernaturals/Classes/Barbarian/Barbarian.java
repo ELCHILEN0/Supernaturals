e9dd47d8c3ccf150ad4d1c6c24f02afc5707fe89
Jnani Weibel <jnaneshwar1234@gmail.com>
2013-07-21 10:59:03 -0700

    Finish Barbarian branch stats

--- src/main/java/com/TeamNovus/Supernaturals/Classes/Barbarian/Barbarian.java
+++ src/main/java/com/TeamNovus/Supernaturals/Classes/Barbarian/Barbarian.java
@@ -9,11 +9,11 @@ public class Barbarian extends SNClass {
 	public Barbarian(SNClass parentClass) {
 		super("Barbarian", ChatColor.DARK_RED, 30, parentClass);
 		
-		setMaxMana(1, 20);
-		setMaxMana(5, 25);
-		setMaxMana(10, 30);
+		setMaxMana(1, 10);
+		setMaxMana(5, 15);
+		setMaxMana(10, 20);
 		setMaxMana(25, 25);
-		setMaxMana(30, 40);
+		setMaxMana(30, 30);
 		
 		setMaxHealth(1, 25);
 		setMaxHealth(5, 26);
@@ -22,12 +22,16 @@ public class Barbarian extends SNClass {
 		setMaxHealth(30, 30);
 		
 		setMaxFoodLevel(1, 20);
+		setMaxFoodLevel(5, 25);
+		setMaxFoodLevel(10, 30);
+		setMaxFoodLevel(25, 35);
+		setMaxFoodLevel(30, 40);
 		
-		setSpeed(1, 0.19f);
-		setSpeed(5, 0.185f);
+		setSpeed(1, 0.165f);
+		setSpeed(5, 0.17f);
 		setSpeed(10, 0.18f);
-		setSpeed(25, 0.17f);
-		setSpeed(30, 0.165f);
+		setSpeed(25, 0.185f);
+		setSpeed(30, 0.19f);
 	}
 
 }
--- src/main/java/com/TeamNovus/Supernaturals/Classes/Barbarian/Classes/Brute/Brute.java
+++ src/main/java/com/TeamNovus/Supernaturals/Classes/Barbarian/Classes/Brute/Brute.java
@@ -9,29 +9,29 @@ public class Brute extends SNClass {
 	public Brute(SNClass parentClass) {
 		super("Brute", ChatColor.DARK_RED, 30, parentClass);
 		
-		setMaxMana(1, 20);
-		setMaxMana(5, 25);
-		setMaxMana(10, 30);
-		setMaxMana(25, 25);
-		setMaxMana(30, 40);
+		setMaxMana(1, 15);
+		setMaxMana(5, 20);
+		setMaxMana(10, 25);
+		setMaxMana(25, 30);
+		setMaxMana(30, 35);
 		
 		setMaxHealth(1, 30);
-		setMaxHealth(5, 33);
-		setMaxHealth(10, 35);
+		setMaxHealth(5, 32);
+		setMaxHealth(10, 34);
 		setMaxHealth(25, 37);
 		setMaxHealth(30, 40);
 		
 		setMaxFoodLevel(1, 20);
-		setMaxFoodLevel(5, 19);
-		setMaxFoodLevel(10, 18);
-		setMaxFoodLevel(25, 17);
-		setMaxFoodLevel(30, 16);
+		setMaxFoodLevel(5, 25);
+		setMaxFoodLevel(10, 30);
+		setMaxFoodLevel(25, 35);
+		setMaxFoodLevel(30, 40);
 		
-		setSpeed(1, 0.17f);
-		setSpeed(5, 0.16f);
+		setSpeed(1, 0.13f);
+		setSpeed(5, 0.14f);
 		setSpeed(10, 0.15f);
-		setSpeed(25, 0.14f);
-		setSpeed(30, 0.13f);
+		setSpeed(25, 0.16f);
+		setSpeed(30, 0.17f);
 	}
 
 }
