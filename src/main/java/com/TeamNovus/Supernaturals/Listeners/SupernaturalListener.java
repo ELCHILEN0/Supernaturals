db39b0069820321dbf1c787af153f832a1350deb
Jnani Weibel <jnaneshwar1234@gmail.com>
2013-07-20 17:49:41 -0700

    Fix PlayerEvents not triggering abilities

--- src/main/java/com/TeamNovus/Supernaturals/Listeners/SupernaturalListener.java
+++ src/main/java/com/TeamNovus/Supernaturals/Listeners/SupernaturalListener.java
@@ -1,16 +1,15 @@
 package com.TeamNovus.Supernaturals.Listeners;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityCombustByBlockEvent;
 import org.bukkit.event.entity.EntityCombustByEntityEvent;
@@ -30,9 +29,6 @@ import com.TeamNovus.Supernaturals.SNEntities;
 import com.TeamNovus.Supernaturals.SNPlayers;
 import com.TeamNovus.Supernaturals.Custom.Effect.Effect;
 import com.TeamNovus.Supernaturals.Custom.Effect.EffectType;
-import com.TeamNovus.Supernaturals.Custom.Effect.EffectTypeListener;
-import com.TeamNovus.Supernaturals.Custom.Effect.EffectTypes.PoisonArrow;
-import com.TeamNovus.Supernaturals.Custom.Effect.EffectTypes.TestEffect;
 import com.TeamNovus.Supernaturals.Events.EntityDamageByEntityProjectileEvent;
 import com.TeamNovus.Supernaturals.Events.EntityDamageEntityByProjectileEvent;
 import com.TeamNovus.Supernaturals.Events.EntityDamageEntityEvent;
@@ -41,114 +37,109 @@ import com.TeamNovus.Supernaturals.Events.EntityEffectExpireEvent;
 import com.TeamNovus.Supernaturals.Events.EntityEffectTickEvent;
 import com.TeamNovus.Supernaturals.Events.EntityEffectTriggerEvent;
 import com.TeamNovus.Supernaturals.Events.PlayerManaChangeEvent;
-import com.TeamNovus.Supernaturals.Player.Class.Ability;
 
 public class SupernaturalListener implements Listener {
 
-//	@EventHandler
-//	public void onPlayerLoginEvent(PlayerLoginEvent event) {
-//		invokeEvent(event, PlayerLoginEvent.class);
-//	}
-//
-//	// Start Effect Events
-//	@EventHandler
-//	public void onEntityEffectBeginEvent(EntityEffectBeginEvent event) {
-//		invokeEvent(event, EntityEffectBeginEvent.class);
-//	}
-//
-//	@EventHandler
-//	public void onEntityEffectTickEvent(EntityEffectTickEvent event) {
-//		invokeEvent(event, EntityEffectTickEvent.class);
-//	}
-//
-//	@EventHandler
-//	public void onEntityEffectTriggerEvent(EntityEffectTriggerEvent event) {
-//		invokeEvent(event, EntityEffectTriggerEvent.class);
-//	}
-//
-//	@EventHandler
-//	public void onEntityEffectExpireEvent(EntityEffectExpireEvent event) {
-//		invokeEvent(event, EntityEffectExpireEvent.class);
-//	}
-//	// End Effect Events:
-//
-//	// Start Mana Change
-//	@EventHandler
-//	public void onPlayerManaChangeEvent(PlayerManaChangeEvent event) {
-//		invokeEvent(event, PlayerManaChangeEvent.class);
-//	}
-//	// End Mana Change
-//
-//	// Start Damage Events
-//	@EventHandler
-//	public void onEntityDamageEvent(EntityDamageEvent event) {
-//		invokeEvent(event, EntityDamageEvent.class);
-//	}
-//
-//	@EventHandler
-//	public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
-//		invokeEvent(event, EntityDamageByEntityEvent.class);
-//	}
-//
-//	@EventHandler
-//	public void onEntityDamageEntityEvent(EntityDamageEntityEvent event) {
-//		invokeEvent(event, EntityDamageEntityEvent.class);
-//	}
-//
-//	@EventHandler
-//	public void onEntityDamageByEntityProjectileEvent(EntityDamageByEntityProjectileEvent event) {
-//		invokeEvent(event, EntityDamageByEntityProjectileEvent.class);
-//	}
-//
-//	@EventHandler
-//	public void onEntityDamageEntityByProjectileEvent(EntityDamageEntityByProjectileEvent event) {
-//		invokeEvent(event, EntityDamageEntityByProjectileEvent.class);
-//	}
-//	// End Damage Events
-//
-//	// Start Player Events
+	// Start Effect Events
+	@EventHandler
+	public void onEntityEffectBeginEvent(EntityEffectBeginEvent event) {
+		invokeEvent(event, EntityEffectBeginEvent.class);
+	}
+
+	@EventHandler
+	public void onEntityEffectTickEvent(EntityEffectTickEvent event) {
+		invokeEvent(event, EntityEffectTickEvent.class);
+	}
+
+	@EventHandler
+	public void onEntityEffectTriggerEvent(EntityEffectTriggerEvent event) {
+		invokeEvent(event, EntityEffectTriggerEvent.class);
+	}
+
+	@EventHandler
+	public void onEntityEffectExpireEvent(EntityEffectExpireEvent event) {
+		invokeEvent(event, EntityEffectExpireEvent.class);
+	}
+
+	// Start Mana Change
+	@EventHandler
+	public void onPlayerManaChangeEvent(PlayerManaChangeEvent event) {
+		invokeEvent(event, PlayerManaChangeEvent.class);
+	}
+
+	// Start Damage Events
+	@EventHandler
+	public void onEntityDamageEvent(EntityDamageEvent event) {
+		invokeEvent(event, EntityDamageEvent.class);
+	}
+
+	@EventHandler
+	public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
+		invokeEvent(event, EntityDamageByEntityEvent.class);
+	}
+
+	@EventHandler
+	public void onEntityDamageEntityEvent(EntityDamageEntityEvent event) {
+		invokeEvent(event, EntityDamageEntityEvent.class);
+	}
+
+	@EventHandler
+	public void onEntityDamageByEntityProjectileEvent(EntityDamageByEntityProjectileEvent event) {
+		invokeEvent(event, EntityDamageByEntityProjectileEvent.class);
+	}
+
+	@EventHandler
+	public void onEntityDamageEntityByProjectileEvent(EntityDamageEntityByProjectileEvent event) {
+		invokeEvent(event, EntityDamageEntityByProjectileEvent.class);
+        System.out.println("SHOOTING EVENT?");
+	}
+
+	// Start Player Events
 	@EventHandler
 	public void onPlayerMoveEvent(PlayerMoveEvent event) {
 		invokeEvent(event, PlayerMoveEvent.class);
 	}
-//
-//	@EventHandler
-//	public void onPlayerRespawnEvent(PlayerRespawnEvent event) {
-//		invokeEvent(event, PlayerRespawnEvent.class);
-//	}
-//	// End Player Events
-//
-//	// Start Entity Events
-//	@EventHandler
-//	public void onEntityShootBowEvent(EntityShootBowEvent event) {
-//		invokeEvent(event, EntityShootBowEvent.class);
-//	}
-//
-//	@EventHandler
-//	public void onEntityCombustEvent(EntityCombustEvent event) {
-//		invokeEvent(event, EntityCombustEvent.class);
-//	}
-//
-//	@EventHandler
-//	public void onEntityCombustByEntityEvent(EntityCombustByEntityEvent event) {
-//		invokeEvent(event, EntityCombustByEntityEvent.class);
-//	}
-//
-//	@EventHandler
-//	public void onEntityCombustByBlockEvent(EntityCombustByBlockEvent event) {
-//		invokeEvent(event, EntityCombustByBlockEvent.class);
-//	}
-//
-//	@EventHandler
-//	public void onEntityTargetEvent(EntityTargetEvent event) {
-//		invokeEvent(event, EntityTargetEvent.class);
-//	}
-//
-//	@EventHandler
-//	public void onEntityTargetLivingEntityEvent(EntityTargetLivingEntityEvent event) {
-//		invokeEvent(event, EntityTargetLivingEntityEvent.class);
-//	}
-	// End EntityEvents
+
+	@EventHandler
+	public void onPlayerRespawnEvent(PlayerRespawnEvent event) {
+		invokeEvent(event, PlayerRespawnEvent.class);
+	}
+
+    @EventHandler
+    public void onPlayerLoginEvent(PlayerLoginEvent event) {
+        invokeEvent(event, PlayerLoginEvent.class);
+    }
+
+	// Start Entity Events
+	@EventHandler
+	public void onEntityShootBowEvent(EntityShootBowEvent event) {
+		invokeEvent(event, EntityShootBowEvent.class);
+	}
+
+	@EventHandler
+	public void onEntityCombustEvent(EntityCombustEvent event) {
+		invokeEvent(event, EntityCombustEvent.class);
+	}
+
+	@EventHandler
+	public void onEntityCombustByEntityEvent(EntityCombustByEntityEvent event) {
+		invokeEvent(event, EntityCombustByEntityEvent.class);
+	}
+
+	@EventHandler
+	public void onEntityCombustByBlockEvent(EntityCombustByBlockEvent event) {
+		invokeEvent(event, EntityCombustByBlockEvent.class);
+	}
+
+	@EventHandler
+	public void onEntityTargetEvent(EntityTargetEvent event) {
+		invokeEvent(event, EntityTargetEvent.class);
+	}
+
+	@EventHandler
+	public void onEntityTargetLivingEntityEvent(EntityTargetLivingEntityEvent event) {
+		invokeEvent(event, EntityTargetLivingEntityEvent.class);
+	}
 
 	/**
 	 * Invoke an event for a BaseEffect for a player or entity.
@@ -159,6 +150,7 @@ public class SupernaturalListener implements Listener {
 	public void invokeEvent(Event event, Class<? extends Event> eventClass) {
 		ArrayList<Effect> effects = new ArrayList<Effect>();
 
+        // Add Entity Effects
 		if (event instanceof EntityEvent) {
 			Entity e = ((EntityEvent) event).getEntity();
 
@@ -167,16 +159,21 @@ public class SupernaturalListener implements Listener {
 			}
 		}
 
+        // Add Player Abilities
 		if (event instanceof PlayerEvent) {
 			effects.addAll(SNPlayers.i.get(((PlayerEvent) event).getPlayer()).getAbilities());
-		}
+		} else if (event instanceof EntityEvent) {
+            Entity e = ((EntityEvent) event).getEntity();
+
+            if(e instanceof Player) {
+                effects.addAll(SNPlayers.i.get((Player) e).getAbilities());
+            }
+        }
 
 		for (Effect effect : effects) {
+            System.out.println(effect);
 			EffectType effectType = effect.getType();
 
-			//			for(EventPriority priority : EventPriority.values()) {
-			//				System.out.println(priority);
-			//			}
 			for (Method method : effectType.getClass().getMethods()) {
 				Type[] types = method.getParameterTypes();
 				Object[] params = new Object[types.length];
@@ -207,8 +204,6 @@ public class SupernaturalListener implements Listener {
                     }
                 }
 			}
-
-			//			}
 		}
 	}
 
