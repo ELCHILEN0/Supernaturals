7b1f3d8e67f552ce76b43973c70155efc0d1620f
Jnani Weibel <jnaneshwar1234@gmail.com>
2013-07-21 17:07:06 -0700

    Finish stats for Savage

--- src/main/java/com/TeamNovus/Supernaturals/Classes/Savage/Savage.java
+++ src/main/java/com/TeamNovus/Supernaturals/Classes/Savage/Savage.java
@@ -1,9 +1,42 @@
 package com.TeamNovus.Supernaturals.Classes.Savage;
 
-public class Savage {
+import org.bukkit.ChatColor;
 
-	public Savage() {
-		// TODO Auto-generated constructor stub
+import com.TeamNovus.Supernaturals.Classes.Savage.Classes.Vampire.Vampire;
+import com.TeamNovus.Supernaturals.Classes.Savage.Classes.Werewolf.Werewolf;
+import com.TeamNovus.Supernaturals.Player.SNClass;
+
+public class Savage extends SNClass {
+
+	public Savage(SNClass parentClass) {
+		super("Savage", ChatColor.GRAY, 30, parentClass);
+		
+		setMaxHealth(1, 25);
+		setMaxHealth(5, 26);
+		setMaxHealth(10, 27);
+		setMaxHealth(25, 29);
+		setMaxHealth(30, 30);
+		
+		setMaxFoodLevel(1, 40);
+		setMaxFoodLevel(5, 45);
+		setMaxFoodLevel(10, 50);
+		setMaxFoodLevel(25, 55);
+		setMaxFoodLevel(30, 60);
+		
+		setSpeed(1, 0.23f);
+		setSpeed(5, 0.24f);
+		setSpeed(10, 0.25f);
+		setSpeed(25, 0.26f);
+		setSpeed(30, 0.27f);
+		
+		setMaxMana(1, 10);
+		setMaxMana(5, 15);
+		setMaxMana(10, 20);
+		setMaxMana(25, 25);
+		setMaxMana(30, 30);
+		
+		addJoinableClass(25, new Vampire(this));
+		addJoinableClass(25, new Werewolf(this));
 	}
-
+	
 }
